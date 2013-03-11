20980fd7c03770c650999b5bcb0a2e9166ba41bf
Nathan Zadoks <nathan@nathan7.eu>
2013-03-11 09:01:22 +0100

    better error behaviour

--- hyperquestionable.js
+++ hyperquestionable.js
@@ -6,15 +6,10 @@ function hyperquestionable(url, opts, cb) {
   if (typeof opts === 'function') cb = opts, opts = null
   cb = once(cb)
   var data = ''
+    , res
   hyperquest(url)
     .on('data', function(chunk) { data += chunk })
-    .on('end', function() { cb(null, data) })
-    .on('error', cb)
-    .on('response', function(res) {
-      if (res.statusCode !== 200) {
-        var err = new Error('HTTP ' + res.statusCode)
-        err.statusCode = res.statusCode
-        cb(err)
-      }
-    })
+    .on('end', function() { cb(null, data, res) })
+    .on('error', function(err) { cb(err, data, res) })
+    .on('response', function(_res) { res = _res })
 }
